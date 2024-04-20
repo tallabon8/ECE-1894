2078139b8b4c1279af6190203e2f38967e81da80
Travis Al-Labon <tma57@pitt.edu>
2024-04-20 19:48:41 -0400

    lights working

--- MAIN.py
+++ MAIN.py
@@ -15,14 +15,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         #deals with initializing to 00:00:00, for time since water refill
         self.timerResetCountWater = []
-        self.sys_time_label.setText("00:00:00")
-        self.sys_time_label_3.setText("00:00:00")
+        #push_Button_3 is the refill water button
+        self.sys_time_label.setText("00:00:00") #time since water fill
+        self.sys_time_label_3.setText("00:00:00") #24hr average time since water fill
         self.elapsedSeconds = 0
         self.sysTimeUpdateTimer = QTimer(self)
         self.sysTimeUpdateTimer.timeout.connect(self.incrementSysTimeLabel)
         self.sysTimeUpdateTimer.start(1000)
         #deals with initializing to 00:00:00, for time since food refill
-        self.sys_time_label_2.setText("00:00:00")
+        #push_Button_2 is the refill food button
+        self.sys_time_label_2.setText("00:00:00") #time since food fill
+        self.sys_time_label_4.setText("00:00:00") #24hr average time since food fill
         self.elapsedSeconds_2 = 0
         self.sysTimeUpdateTimer = QTimer(self)
         self.sysTimeUpdateTimer.timeout.connect(self.incrementSysTimeLabel_2)
@@ -39,18 +42,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.pushButton_4.clicked.connect(self.incrementLcdNumber2)
         #Reset Egg Count
         self.pushButton.clicked.connect(self.resetLcdNumber2)
-        #Record Egg Count
-        # self.lcdNumber2Values = []
-        # # Setup the timer to record the lcdNumber_2 value every hour
-        # self.recordValueTimer = QTimer(self)
-        # self.recordValueTimer.timeout.connect(self.recordLcdNumber2Value)
-        # self.recordValueTimer.start(3600000)  # Every hour
-        # # Setup another timer to save the data to CSV every 24 hours
-        # self.saveDataTimer = QTimer(self)
-        # self.saveDataTimer.timeout.connect(self.saveLcdNumber2ValuesToCsv)
-        # self.saveDataTimer.start(86400000)  # Every 24 hours
-
-
+        self.ebrake_fail_on = self.convertToClickableLabel(self.ebrake_fail_on)
+        self.ebrake_fail_off = self.convertToClickableLabel(self.ebrake_fail_off)
+        # Connect the clickable labels to functions
+        self.ebrake_fail_on.clicked.connect(self.lightsOn)
+        self.ebrake_fail_off.clicked.connect(self.lightsOff)
 
     def incrementSysTimeLabel(self):
         # Increment the elapsed time by 1 second
@@ -66,19 +62,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         timeToDisplay = QTime(0, 0, 0).addSecs(self.elapsedSeconds_2)
         # Update the sys_time_label with the new time
         self.sys_time_label_2.setText(timeToDisplay.toString("hh:mm:ss"))
+    def calculateAverageTime(self):
+        totalSeconds = 0
+        for time in self.timerResetCountWater:
+            secondsSinceMidnight = QTime(0, 0, 0).secsTo(time.time())
+            totalSeconds += secondsSinceMidnight
+        averageSeconds = totalSeconds / len(self.timerResetCountWater)
+        averageTime = QTime(0, 0, 0).addSecs(int(averageSeconds))
+        return averageTime
 
     def startRefillWater(self):
-        averageTime = self.calculateDisplayAvgWater()  # Get the average time
-        if not self.timerResetCountWater:  # If no refills have been logged, use current time
-            self.timerResetCountWater.append(QDateTime.currentDateTime())
+        self.sys_time_label_3.setText("00:00:00")
 
-        # Assuming you want to reset the timer to reflect the average time elapsed since midnight
-        elapsedSinceMidnight = QTime(0, 0, 0).secsTo(averageTime)
-        self.elapsedSeconds = elapsedSinceMidnight
-        self.sys_time_label.setText(averageTime.toString("hh:mm:ss"))
-        self.updateSysTimeLabel = True
-
-        
     def calculateDisplayAvgWater(self):
         if not self.timerResetCountWater:
             self.sys_time_label_3.setText("00:00:00")
@@ -93,43 +88,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         averageTime = QTime(0, 0, 0).addSecs(int(averageSeconds))
         self.sys_time_label_3.setText(averageTime.toString("hh:mm:ss"))
         return averageTime  # Return the calculated average time
-
-
-
-
+    
+    def calculateDisplayAvgFood(self):
+        if not self.timerResetCountFood:
+            self.sys_time_label_4.setText("00:00:00")
+            return QTime(0, 0, 0)  # Return default QTime if list is empty
+        
+        totalSeconds = 0
+        for time in self.timerResetCountFood:
+            secondsSinceMidnight = QTime(0, 0, 0).secsTo(time.time())
+            totalSeconds += secondsSinceMidnight
+        
+        averageSeconds = totalSeconds / len(self.timerResetCountFood)
+        averageTime = QTime(0, 0, 0).addSecs(int(averageSeconds))
+        self.sys_time_label_4.setText(averageTime.toString("hh:mm:ss"))
+        return averageTime  # Return the calculated average time
 
     def startRefillFood(self):
-        now = QDateTime.currentDateTime()
-        self.timerResetCountWater.append(now)
-        self.calculateDisplayAvgWater()
-        self.elapsedSeconds_2 = 0
-        self.sys_time_label_2.setText("00:00:00")
-        self.updateSysTimeLabel2 = False
+        self.sys_time_label_4.setText("00:00:00")
         
     def msecs(self, time):
         return ((time.hour() * 3600) + (time.minute() * 60) + time.second()) * 1000 + time.msec()
-    
-    # def calculateDisplayAvg(self):
-    #     # Calculate the QDateTime for 24 hours ago
-    #     twentyFourHoursAgo = QDateTime.currentDateTime().addSecs(-86400)  # 24 hours in seconds
-        
-    #     # Keep only QDateTime objects in the list that are newer than 24 hours ago
-    #     self.timerResetCount1 = [time for time in self.timerResetCount1 if time > twentyFourHoursAgo]
-
-    #     # If the list is empty after filtering, you might want to handle this case separately
-    #     if not self.timerResetCount1:
-    #         # Handle the case where no resets have occurred in the last 24 hours
-    #         self.sys_time_label_2.setText("00:00:00")
-    #         return
-    #     memmy = sum(self.msecs(time.time()) for time in self.timerResetCount1)
-    #     # Calculate average time in milliseconds since midnight
-    #     averageMilliseconds = memmy / len(self.timerResetCount1)
-        
-    #     # Convert average milliseconds to QTime and display
-    #     averageTime = QTime(0, 0, 0).addMSecs(int(averageMilliseconds))
-    #     self.sys_time_label_2.setText(averageTime.toString("hh:mm:ss"))
-    #     self.sys_time_label_4.setText(averageTime.toString("hh:mm:ss"))
-
 
     def updateRefillDuration(self):
         # Calculate the duration since the last refill start
@@ -150,15 +129,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Connect button clicks to their respective functions
         self.pushButton.clicked.connect(self.resetLcdNumber2)
-        self.pushButton_2.clicked.connect(lambda: self.toggleSysTimeLabelUpdate(False))  # Pass False to stop updating
-        self.pushButton_3.clicked.connect(lambda: self.toggleSysTimeLabel2Update(False))  # Pass False to stop updating
+        self.pushButton_2.clicked.connect(self.calculateDisplayAvgFood)
+        self.pushButton_3.clicked.connect(self.calculateDisplayAvgWater)
         self.pushButton_4.clicked.connect(self.incrementLcdNumber2)
         
         self.ebrake_fail_off = self.convertToClickableLabel(self.ebrake_fail_off)
         self.ebrake_fail_on = self.convertToClickableLabel(self.ebrake_fail_on)
 
-        self.ebrake_fail_off.clicked.connect(self.lightsOff)
-        self.ebrake_fail_on.clicked.connect(self.lightsOn)
+ 
         
         # Timer for updating sys_time_labels with time
         self.timer = QTimer(self)
@@ -216,25 +194,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.textEdit_2.clear()
         else:
             print("Please enter a serial number and device name")
-
-# def recordLcdNumber2Value(self):
-#     current_value = self.lcdNumber_2.value()
-#     current_time = QDateTime.currentDateTime().toString("yyyy-MM-dd HH:mm:ss")
-#     self.lcdNumber2Values.append((current_time, current_value))
-
-# def saveLcdNumber2ValuesToCsv(self):
-#     # Define the CSV file name
-#     file_name = "egg_stats.csv"
-#     with open(file_name, 'w', newline='') as file:
-#         writer = csv.writer(file)
-#         # Write a header row
-#         writer.writerow(["Timestamp", "Value"])
-#         # Write the data
-#         writer.writerows(self.lcdNumber2Values)
-    
-#     # Optionally, clear the list after saving
-#     self.lcdNumber2Values.clear()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
